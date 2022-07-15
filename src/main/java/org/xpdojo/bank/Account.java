c5ec40a866ffe659e6af336b9c0f59d04c5643a1
Nas <nas@breaking-studio.com>
2022-07-15 14:25:44 +0200

    first test

--- src/main/java/org/xpdojo/bank/Account.java
+++ src/main/java/org/xpdojo/bank/Account.java
@@ -1,4 +1,16 @@
 package org.xpdojo.bank;
 
 public class Account {
+
+     int balance = 0;
+
+    void initialize(int value) {
+        balance = 0;
+    }
+
+    int getBalance() {
+       return balance;
+    }
+
+
 }
--- src/test/java/org/xpdojo/bank/AccountTest.java
+++ src/test/java/org/xpdojo/bank/AccountTest.java
@@ -1,16 +1,24 @@
 package org.xpdojo.bank;
 
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import static org.hamcrest.CoreMatchers.*;
+
 
 public class AccountTest {
 
     @Test
-	@Disabled
-    public void depositAnAmountToIncreaseTheBalance() {
-        assertThat("your first test").isBlank();
+    public void accountInitialize() {
+        Account acc = new Account();
+        acc.initialize(0);
+        assertThat(acc.getBalance(), is(0));
     }
+
+
+
 
 }
