4a19ef218cb1a1dcb580ae44354b86416d076598
Nas <nas@breaking-studio.com>
2022-07-15 14:38:35 +0200

    depositMoney

--- src/test/java/org/xpdojo/bank/AccountTest.java
+++ src/test/java/org/xpdojo/bank/AccountTest.java
@@ -1,22 +1,17 @@
 package org.xpdojo.bank;
 
-
-
-import org.junit.Test;
-
+import org.junit.jupiter.api.Test;
 import static org.hamcrest.MatcherAssert.assertThat;
-
 import static org.hamcrest.CoreMatchers.*;
 
-
 public class AccountTest {
+
 
     @Test
     public void accountInitialize() {
         Account acc = new Account();
         assertThat(acc.getBalance(), is(0));
     }
-
     @Test
     public void depositMoney() {
         Account acc = new Account();
